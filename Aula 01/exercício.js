b3b11a132964d006bee7ab5d1d5de9f15fd0a0f7
Gabriela Silva <163335365+gabipsilvaofc@users.noreply.github.com>
2024-04-08 16:49:54 -0300

    Aula 01

---
+++ Aula 01/exercício.js
@@ -0,0 +1,16 @@
+alert('Oi, eu sou o exercicio e estou muito feliz em ver você!')
+
+let nome 
+let sobrenome 
+
+nome = prompt('qual o seu nome?')
+console.log('Nome completo: ' + nome)
+ 
+sobrenome = prompt('qual seu sobrenome')
+console.log('Nome completo: ' + sobrenome)
+
+
+let nomeCompleto
+nomeCompleto = nome + ' ' + sobrenome
+console.log('Nome completo: ' + nomeCompleto)
+alert('Seu nome é: ' + nomeCompleto)
---
+++ Aula 01/script.js
@@ -0,0 +1,51 @@
+
+// let resposta 
+
+// let (representa uma variável) idade
+// Estou pronto prompt
+// resposta = prompt('Qual o seu nome?')
+
+// alert (resposta)
+// alert ('Seu nome é: ' + resposta)
+
+let idade
+idade = 17 
+console.log(idade);
+
+idade = idade + 6
+// isso é uma equação
+console.log('somando 6: ' + idade);
+
+// idade = idade + '1'
+// console.log(idade)
+// juntou com o número 1, não somou. por causa das aspas.
+
+// idade = idade + 1 pode ser escrito como
+idade++
+console.log('Incremento: ' + idade);
+
+// idade += 5 simplificando o "idade = idade + x"
+
+
+// subtração
+let idadeNo7a1
+idadeNo7a1 = idade - 10
+console.log('idade no 7x1: ' + idadeNo7a1);
+
+// multiplicação
+let dobro
+dobro = idade * 2
+console.log('dobrado: ' + dobro);
+
+console.log('Cada um de vocês me deve R$' + idade); // cueca sendo usada no pé (ou seja, não faz sentido)
+
+// divisão
+let metade 
+metade = idade / 2
+console.log('Metadinha: ' + metade);
+
+// potência 
+let Quadrado
+Quadrado = idade ** 2
+console.log('Quadrado: ' + Quadrado);
+
